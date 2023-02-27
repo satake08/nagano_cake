119157ee5600faed0a526d709eba17c3f29aa000
satake08 <117998354+satake08@users.noreply.github.com>
2023-02-27 16:07:58 +0000

    [add]コントローラー・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,18 @@ Rails.application.routes.draw do
     get 'customers/show'
     get 'customers/edit'
   end
-  devise_for :admins
-  devise_for :customers
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
