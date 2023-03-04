562fe93b676a90b3e7dbd0fa5cdf9902280c8fe9
satake08 <117998354+satake08@users.noreply.github.com>
2023-03-04 16:54:22 +0000

    [add]ヘッダ作り始め

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,6 +13,11 @@ class Public::CustomersController < ApplicationController
   end
   
   def withdraw
-    
+    @customer = User.find(params[:id])
+    # is_activeカラムをfalseに変更することにより削除フラグを立てる
+    @customer.update(is_active: false)
+    reset_session
+    #flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,29 @@
 Rails.application.routes.draw do
-  namespace :public do
+  scope module: :public do
     get 'addresses/index'
     get 'addresses/edit'
   end
-  namespace :public do
+  scope module: :public do
     get 'orders/new'
     get 'orders/complete'
     get 'orders/index'
     get 'orders/show'
   end
-  namespace :public do
+  scope module: :public do
     get 'cart_items/index'
   end
-  namespace :public do
+  scope module: :public do
     get 'customers/show'
     get 'customers/edit'
     get 'customers/confirm'
   end
-  namespace :public do
+  scope module: :public do
     get 'items/index'
     get 'items/show'
   end
-  namespace :public do
-    get 'homes/top'
+  scope module: :public do
+    #get 'homes/top'
+    root to: "homes#top"
     get 'homes/about'
   end
   namespace :admin do
@@ -39,25 +40,34 @@ Rails.application.routes.draw do
     get 'items/edit'
   end
   namespace :admin do
-    get 'homes/top'
+    #get 'homes/top'
+    #root to: "homes#top"
   end
   namespace :admin do
+    #root :to => "devise/sessions#new"
+    #root to: 'customers#sign_in'
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
   end
-  
+
   # 管理者用
   # URL /admin/sign_in ...
+  # rootをログイン画面に設定
+  devise_scope :admin do
+    root "admin/sessions#new"
+  end
+  
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
